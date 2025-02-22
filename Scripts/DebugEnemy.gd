d87773d7e6ccbee4f7211444856a69dcda874129
Benjamin Belnap <ben.belnap21@gmail.com>
2025-02-21 17:51:51 -0700

    fixed state machine

--- Scripts/DebugEnemy.gd
+++ Scripts/DebugEnemy.gd
@@ -8,6 +8,7 @@ enum State { IDLE, ATTACK }	# Define states
 
 @export var current_state = State.IDLE
 var player = null
+var on_cooldown = false
 
 func _ready():
 	$Timer.wait_time = attack_interval
@@ -21,35 +22,39 @@ func _ready():
 func _on_player_detected(new_player):
 	player = new_player
 	change_state(State.ATTACK)
-	print("attack")
+
 
 func _on_player_lost():
 	player = null
 	change_state(State.IDLE)
-	print("boring")
+
 
 
 func _process(delta):
 	match current_state:
 		State.IDLE:
-			look_for_player()
+			#look_for_player()
+			pass
 		State.ATTACK:
-			attack()
+			if !on_cooldown:
+				attack()
 
-func look_for_player():
-	if player and global_position.distance_to(player.global_position) < attack_range:
-		change_state(State.ATTACK)
+#func look_for_player():
+	#if player and global_position.distance_to(player.global_position) < attack_range:
+		#change_state(State.ATTACK)
 
 func attack():
 	print("Enemy attacks!")  # Replace with actual attack logic
 	# Emit signal here if needed, like EventBus.emit_signal("enemy_attack")
 
 	$Timer.start()	# Restart attack timer
+	on_cooldown = true
 
 func change_state(new_state):
 	current_state = new_state
 
 # Called when timer times out (Repeat attacks)
-func _on_Timer_timeout():
-	if current_state == State.ATTACK:
-		attack()
+func _on_timer_timeout():
+	on_cooldown = false
+
+
