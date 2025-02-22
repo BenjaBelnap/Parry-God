3b24603d3b43719e0d33975fd1c9b777774c2dbf
Derek Thompson <DerekAce756@gmail.com>
2025-02-22 15:15:01 -0700

    Added main menu

--- Scripts/GameBoard.gd
+++ Scripts/GameBoard.gd
@@ -5,13 +5,11 @@ extends Node2D
 @onready var game_over: Control = $"Game Over"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
 
 
 func _on_player_death():
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,12 +1,21 @@
-extends Node2D
-
-
+extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$VBoxContainer/StartButton.grab_focus()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Senes/game_board.tscn")
+
+
+func _on_option_button_pressed() -> void:
+	print("Bring up options.")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
