1a2e3d5c81ffc5b9e0c76aa5c04416ae02ea63b1
Derek Thompson <DerekAce756@gmail.com>
2025-02-22 22:13:57 -0700

    Refactored animations

--- Scripts/DebugEnemy.gd
+++ Scripts/DebugEnemy.gd
@@ -80,15 +80,6 @@ func attack():
 	print("Enemy attacks!")  
 	on_cooldown = true
 	animationPlayer.play("swipe")  # Play attack animation
-	
-	# Enable attack hitbox briefly
-	await get_tree().create_timer(0.4).timeout
-	attackHitbox.monitoring = true
-	await get_tree().create_timer(0.1).timeout
-	swing_audio.play()
-	await get_tree().create_timer(0.3).timeout
-	attackHitbox.monitoring = false
-
 	attackTimer.start()
 
 func _on_attack_hit(body):
