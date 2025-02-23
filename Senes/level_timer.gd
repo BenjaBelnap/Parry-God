c4d09f1478d781a91cb60093ee5571edd3e63988
Benjamin Belnap <ben.belnap21@gmail.com>
2025-02-22 17:24:33 -0700

    changed timer data

--- Senes/level_timer.gd
+++ Senes/level_timer.gd
@@ -11,6 +11,7 @@ signal time_up
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimationPlayer.play("start")
+	total_time = Global.level_data[Global.level]["time"]
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
--- global.gd
+++ global.gd
@@ -2,6 +2,8 @@ extends Node
 
 var level: int
 
+var level_data:Array = [{"time":240},{"time":100}]
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
